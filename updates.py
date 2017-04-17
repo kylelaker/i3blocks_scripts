8d5ce1d0346416d841996e1a082f08932cab497a
Kyle Laker <lakerka@dukes.jmu.edu>
2017-04-17 19:55:40 -0400

    updates.py: Style "fixes"

--- updates.py
+++ updates.py
@@ -5,18 +5,21 @@ import subprocess
 
 data = feedparser.parse('https://www.archlinux.org/feeds/news/')
 
-updatesProc = subprocess.Popen("pacaur -Qu | cut -d ' ' -f 1 | tr '\n' ' '", stdout=subprocess.PIPE, shell=True)
-updates = updatesProc.stdout.read()
+updatesList = subprocess.Popen("pacaur -Qu | cut -d ' ' -f 1 | tr '\n' ' '",
+        stdout=subprocess.PIPE,
+        shell=True).stdout.read().split()
 
-updatesList = updates.split()
+exclamation = ""
+downArrow = ""
+checkMark = ""
 
 for update in updatesList:
     for post in data.entries[0:2]:
         if update in post:
-            print("<span color='#FF0000'></span>")
+            print("<span color='#FF0000'>%s</span>" % exclamation)
             quit()
 
 if len(updatesList) > 0:
-    print("<span color='#FFF600'></span>")
+    print("<span color='#FFF600'>%s</span>" % downArrow)
 else:
-    print("<span color='#00FF00'></span>")
+    print("<span color='#00FF00'>%s</span>" % checkMark)
