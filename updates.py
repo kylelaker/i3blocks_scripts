a782819129ad00bbb72db09d470249f8e0184a5d
Kyle Laker <lakerka@dukes.jmu.edu>
2017-07-30 11:02:03 -0400

    (updates.py) Proper colors and spacing

--- updates.py
+++ updates.py
@@ -9,17 +9,17 @@ updatesList = subprocess.Popen("pacaur -Qu | cut -d ' ' -f 1 | tr '\n' ' '",
         stdout=subprocess.PIPE,
         shell=True).stdout.read().split()
 
-exclamation = ""
-downArrow = ""
-checkMark = ""
+exclamation = " "
+downArrow = " "
+checkMark = " "
 
 for update in updatesList:
     for post in data.entries[0:2]:
         if update in post:
-            print("<span color='#FF0000'>%s</span>" % exclamation)
+            print("<span color='#dc322f'>%s</span>" % exclamation)
             quit()
 
 if len(updatesList) > 0:
-    print("<span color='#FFF600'>%s</span>" % downArrow)
+    print("<span color='#b58900'>%s</span>" % downArrow)
 else:
-    print("<span color='#00FF00'>%s</span>" % checkMark)
+    print("<span color='#859900'>%s</span>" % checkMark)
