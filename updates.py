19886d9941ef5fe24940b7a9fa29890bbdc26923
Kyle Laker <kyle@laker.email>
2018-09-30 19:40:13 -0400

    (updates) Cleanup Python

--- updates.py
+++ updates.py
@@ -1,25 +1,36 @@
 #!/usr/bin/env python3
 
+"""
+Script to check availability of updates on ArchLinux. This also checks to see
+if there are any recent news entries
+"""
+
 import feedparser
 import subprocess
 
-data = feedparser.parse('https://www.archlinux.org/feeds/news/')
+def main():
+    data = feedparser.parse('https://www.archlinux.org/feeds/news/')
 
-updatesList = subprocess.Popen("pacaur -Qu | cut -d ' ' -f 1 | tr '\n' ' '",
+    updatesList = subprocess.Popen(
+        "pacaur -Qu | cut -d ' ' -f 1 | tr '\n' ' '",
         stdout=subprocess.PIPE,
-        shell=True).stdout.read().split()
+        shell=True
+    ).stdout.read().split()
 
-exclamation = " "
-downArrow = " "
-checkMark = " "
+    exclamation = " "
+    downArrow = " "
+    checkMark = " "
 
-for update in updatesList:
-    for post in data.entries[0:2]:
-        if update in post:
-            print("<span color='#dc322f'>%s</span>" % exclamation)
-            quit()
+    for update in updatesList:
+        for post in data.entries[0:2]:
+            if update in post:
+                print("<span color='#dc322f'>%s</span>" % exclamation)
+                quit()
 
-if len(updatesList) > 0:
-    print("<span color='#b58900'>%s</span>" % downArrow)
-else:
-    print("<span color='#859900'>%s</span>" % checkMark)
+    if len(updatesList) > 0:
+        print("<span color='#b58900'>%s</span>" % downArrow)
+    else:
+        print("<span color='#859900'>%s</span>" % checkMark)
+
+if __name__ == '__main__':
+    main()
